e913fd3e477bd51bd3c6860511eb8fc26971361f
John Jayvee <jayveeborre0103@gmail.com>
2026-03-17 17:20:55 +0800

    Add files via upload

---
+++ MyLib/Lot.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package MyLib;
+
+/**
+ *
+ * @author Jayvee
+ */
+class Lot {
+    private House house;
+    private int lotNumbers;
+    private double squareMeters;
+    private boolean isInner;
+    private double totalValue;
+    private boolean status;
+    
+    public String returnBlockLotNumbers() {
+        return "";
+    }
+}
---
+++ MyLib/User.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package MyLib;
+
+/**
+ *
+ * @author Jayvee
+ */
+public class User {
+    private Customer[] customer = new Customer[10];
+    private Agent[] agent = new Agent[1000];
+    private Admin admin = new Admin();
+    private static int customerCnt = 0; // length and indexing GMI
+    private static int agentCnt = 0;
+    private int lotHover;
+    private int loggedIn;
+
+    public void register(int role, String username, String password) {
+        switch (role) {
+            case 1:
+            {
+                System.out.println("[SUCCESS]: Created new user " + agentCnt + " : " + username + " - " + password);
+                agent[agentCnt] = new Agent("Agent", username, password);
+                agentCnt++;
+                break;
+            }
+            case 2:
+            {
+                System.out.println("[SUCCESS]: Created new user " + customerCnt + " : " + username + " - " + password);
+                customer[customerCnt] = new Customer("Customer", username, password);
+                customerCnt++;
+                break;
+            }
+        }
+    }
+    
+    public void login() {
+        
+    }
+    
+    public void logout() {
+        
+    }
+
+    public Customer[] getCustomer() {
+        return customer;
+    }
+
+    public Agent[] getAgent() {
+        return agent;
+    }
+
+    public static int getCustomerCnt() {
+        return customerCnt;
+    }
+
+    public static int getAgentCnt() {
+        return agentCnt;
+    }
+}
